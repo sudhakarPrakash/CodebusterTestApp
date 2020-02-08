286251b0718f00709bc635c8a00257e6e8ebe2b3
sudhakar <sudhakarprakash108@gmail.com>
2020-02-08 13:21:31 +0530

    search hospital and nearby hospitals are working till now

--- app/src/main/java/com/example/testapp/HospitalActivity.java
+++ app/src/main/java/com/example/testapp/HospitalActivity.java
@@ -6,21 +6,32 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.material.appbar.AppBarLayout;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
 
-public class HospitalActivity extends AppCompatActivity {
+import static com.example.testapp.Constants.MAP_VIEW_BUNDLE_KEY;
+
+public class HospitalActivity extends AppCompatActivity implements OnMapReadyCallback{
 
     Connection connection;
     String markRecent;
 
     AppBarLayout appBarLayout;
+
+    private MapView mapView;
+    private GoogleMap gmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +41,15 @@ public class HospitalActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         appBarLayout = findViewById(R.id.app_bar);
+
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAP_VIEW_BUNDLE_KEY);
+        }
+
+        mapView = findViewById(R.id.map_view);
+        mapView.onCreate(mapViewBundle);
+        mapView.getMapAsync(this);
 
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
@@ -65,5 +85,61 @@ public class HospitalActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
+    }
+
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Bundle mapViewBundle = outState.getBundle(MAP_VIEW_BUNDLE_KEY);
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle(MAP_VIEW_BUNDLE_KEY, mapViewBundle);
+        }
+
+        mapView.onSaveInstanceState(mapViewBundle);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+    @Override
+    protected void onPause() {
+        mapView.onPause();
+        super.onPause();
+    }
+    @Override
+    protected void onDestroy() {
+        mapView.onDestroy();
+        super.onDestroy();
+    }
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        gmap = googleMap;
+        gmap.setMinZoomPreference(12);
+        LatLng ny = new LatLng(40.7143528, -74.0059731);
+        gmap.moveCamera(CameraUpdateFactory.newLatLng(ny));
     }
 }
